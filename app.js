b215eb87f1f8fbcd7ccd076073b43c0f84b0e466
Robert <rlhead94@gmail.com>
2023-02-13 20:33:14 -0500

    variables hw

---
+++ app.js
@@ -0,0 +1,41 @@
+var firstVar = 1
+var secondVar = 2
+var thirdVar = 3
+var fourthVar = 4
+var fifthVar = 5
+var sixthVar = 6
+var seventhVar = 7
+var eigthVar = 8
+var ninthVar = 9
+var tenthVar = 10
+function addSum (a,b){
+    console.log(a + b)
+}
+addSum (firstVar, secondVar)
+
+addSum (fifthVar, ninthVar)
+
+function subtract (c,d) {
+    console.log (c - d)
+}
+
+subtract (sixthVar, ninthVar)
+subtract (eigthVar, tenthVar)
+function multiply (e,f){
+    console.log(e - f)
+}
+multiply (fifthVar, tenthVar)
+multiply (secondVar,fourthVar)
+
+function divide(g,h){
+    console.log(g / h)
+}
+divide (sixthVar,secondVar)
+divide (eigthVar, fourthVar)
+
+console.log(secondVar++)
+console.log(thirdVar++)
+
+console.log(fourthVar--)
+console.log(fifthVar--)
+
